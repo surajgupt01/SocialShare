f4ddf8aa8e43f81efab382c04b7af677b002ab15
surajgupt01 <surajgupt880@gmail.com>
2025-03-22 10:56:04 +0530

    origin fix

--- SecoundBrain/src/index.ts
+++ SecoundBrain/src/index.ts
@@ -34,12 +34,18 @@ async function Connect(){
 Connect()
 
 
+if(!process.env.REQURL){
+    
+}
 
-const corsOptions = {
-    origin: 'https://https://social-share-one.vercel.app/',
-    methods: 'GET,POST', // Allow only these methods
-    allowedHeaders: ['Content-Type', 'Authorization'] // Allow only these headers
-};
+    const corsOptions = {
+        origin:  process.env.REQURL,
+        methods: 'GET,POST', 
+        allowedHeaders: ['Content-Type', 'Authorization'] 
+    };
+
+
+
 
 app.use(cors(corsOptions))
 
