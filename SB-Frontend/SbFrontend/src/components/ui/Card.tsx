580fdb426c65d81bc48256b9143189dde901a9f0
surajgupt01 <surajgupt880@gmail.com>
2025-04-20 04:11:38 +0530

    homepage

--- SB-Frontend/SbFrontend/src/components/ui/Card.tsx
+++ SB-Frontend/SbFrontend/src/components/ui/Card.tsx
@@ -68,9 +68,9 @@ export default function Card({type, link, title , _id , date } : CardProps){
 
     return(
   
-      <div className="border-1 rounded-md border-gray-800 md:p-2 p-2 shadow-md overflow-y-auto no-scrollbar col-span-1 max-h-100">
+      <div className='border-1 border-gray-300 w-90 h-65  rounded-xl relative flex justify-center items-center'>
   
-       <div className={`md:text-xl text-lg font-semibold flex justify-between ${colorClass} `} >{type.toUpperCase()}
+       <div className={`md:text-xl text-lg font-semibold flex justify-between `} >
 
        <div className=" flex p-2 text-gray-500 cursor-pointer justify-between">
        <button className="mr-3 cursor-pointer" onClick={()=>{
@@ -81,35 +81,21 @@ export default function Card({type, link, title , _id , date } : CardProps){
           deleteHandler()
         }}><Bin/></button>
         </div> 
-      </div>
-      <div className="md:text-2xl text-2xl font-semibold md:mt-5 mt-2 text-gray-500">{title}</div>
+        </div>
+        <div className="md:text-2xl text-2xl font-semibold md:mt-5 mt-2 text-gray-500">{title}</div>
      
   
       
-     {  type==="youtube" && <iframe src = {link.replace("watch" , "embed").replace("?v=" , "/")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen={true} className="w-full md:h-50 h-35 rounded-md mt-5 border-1 border-gray-500 cursor-pointer"></iframe>}
-     {type==="twitter" && <blockquote className="twitter-tweet w-full max-h-200 overflow-hidden text-ellipsis rounded-md mt-5 border border-gray-500 cursor-pointer">
-    <a href={link.replace("x.com", "twitter.com")}></a>
-  </blockquote>}
-  {type==='docs' && <iframe src={link.replace('view' , 'preview')} className="w-full h-50 rounded-md mt-5 border border-gray-500 cursor-pointer" ></iframe>}
+        {  type==="youtube" && <iframe src = {link.replace("watch" , "embed").replace("?v=" , "/")} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen={true} className="w-full md:h-50 h-35 rounded-md mt-5 border-1 border-gray-500 cursor-pointer"></iframe>}
+       {type==="twitter" && <blockquote className="twitter-tweet w-full max-h-200 overflow-hidden text-ellipsis rounded-md mt-5 border border-gray-500 cursor-pointer">
+        <a href={link.replace("x.com", "twitter.com")}></a>
+        </blockquote>}
+       {type==='docs' && <iframe src={link.replace('view' , 'preview')} className="w-full h-50 rounded-md mt-5 border border-gray-500 cursor-pointer" ></iframe>}
+
 
   
+       <div className="text-gray-400 text-sm mt-2"> Added on - {date}</div>
   
-  
-      <div className="flex md:mt-2 mt-2 ">
-
-
-      <div className="mr-2 bg-blue-200 rounded-lg p-1">#Productivity</div>
-
-        <div className="mr-2 bg-blue-200 rounded-lg p-1"> #Ideas</div>
- 
-
-
-           
-      
       </div>
-  
-      <div className="text-gray-400 text-sm mt-2"> Added on - {date}</div>
-  
-    </div>
     )
   }
